5837c9c702b21ddfd310b03c479c1a62069261bb
ytakahashi <yt01tc@gmail.com>
2020-06-21 20:24:18 +0900

    update tests

--- pkg/date/formatter_test.go
+++ pkg/date/formatter_test.go
@@ -1,9 +1,116 @@
 package date
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
+
+func Test_NewInputFormatter_dateInputFormatter(t *testing.T) {
+	input := "06-10"
+
+	actual := NewInputFormatter(input)
+
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(dateInputFormatter{})
+
+	actualFormatter := actual.(dateInputFormatter)
+	if actualType != expectedType {
+		t.Errorf("Error. expect: %s actual: %s", expectedType, actualType)
+	}
+	if actualFormatter.input != input {
+		t.Errorf("Error. expect: %s actual: %s", input, actualFormatter.input)
+	}
+}
+
+func Test_NewInputFormatter_timeInputFormatter_second(t *testing.T) {
+	input := "12:13:14"
+
+	actual := NewInputFormatter(input)
+
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(timeInputFormatter{})
+
+	if actualType != expectedType {
+		t.Errorf("Error. expect: %s actual: %s", expectedType, actualType)
+	}
+
+	actualFormatter := actual.(timeInputFormatter)
+	if actualFormatter.input != input {
+		t.Errorf("Error. expect: %s actual: %s", input, actualFormatter.input)
+	}
+	if actualFormatter.precision != "second" {
+		t.Errorf("Error. actual: %s", actualFormatter.precision)
+	}
+}
+
+func Test_NewInputFormatter_timeInputFormatter_minute(t *testing.T) {
+	input := "12:13"
+
+	actual := NewInputFormatter(input)
+
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(timeInputFormatter{})
+
+	if actualType != expectedType {
+		t.Errorf("Error. expect: %s actual: %s", expectedType, actualType)
+	}
+
+	actualFormatter := actual.(timeInputFormatter)
+	if actualFormatter.input != input {
+		t.Errorf("Error. expect: %s actual: %s", input, actualFormatter.input)
+	}
+	if actualFormatter.precision != "minute" {
+		t.Errorf("Error. actual: %s", actualFormatter.precision)
+	}
+}
+
+func Test_NewInputFormatter_dateTimeInputFormatter(t *testing.T) {
+	input := "2020-06-10T12:34:56Z"
+
+	actual := NewInputFormatter(input)
+
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(dateTimeInputFormatter{})
+
+	actualFormatter := actual.(dateTimeInputFormatter)
+	if actualType != expectedType {
+		t.Errorf("Error. expect: %s actual: %s", expectedType, actualType)
+	}
+	if actualFormatter.input != input {
+		t.Errorf("Error. expect: %s actual: %s", input, actualFormatter.input)
+	}
+}
+
+func Test_NewInputFormatter_unixTimeInputFormatter(t *testing.T) {
+	input := "1592724053"
+
+	actual := NewInputFormatter(input)
+
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(unixTimeInputFormatter{})
+
+	actualFormatter := actual.(unixTimeInputFormatter)
+	if actualType != expectedType {
+		t.Errorf("Error. expect: %s actual: %s", expectedType, actualType)
+	}
+	if actualFormatter.input != input {
+		t.Errorf("Error. expect: %s actual: %s", input, actualFormatter.input)
+	}
+}
+
+func Test_NewInputFormatter_currentTimeFormatter(t *testing.T) {
+	input := "a"
+
+	actual := NewInputFormatter(input)
+
+	actualType := reflect.TypeOf(actual)
+	expectedType := reflect.TypeOf(currentTimeFormatter{})
+
+	if actualType != expectedType {
+		t.Errorf("Error. expect: %s actual: %s", expectedType, actualType)
+	}
+}
 
 func Test_newTimeStruct(t *testing.T) {
 	v := "test"
--- pkg/date/time_test.go
+++ pkg/date/time_test.go
@@ -1,8 +1,11 @@
 package date
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func Test_ToTime_hhmmss(t *testing.T) {
+func Test_timeInputFormatter_ToTime_hhmmss(t *testing.T) {
 	value := "01:01:00Z"
 
 	sut := timeInputFormatter{
@@ -26,7 +29,7 @@ func Test_ToTime_hhmmss(t *testing.T) {
 	}
 }
 
-func Test_ToTime_hhmmss_local(t *testing.T) {
+func Test_timeInputFormatter_ToTime_hhmmss_local(t *testing.T) {
 	value := "10:01:00+09:00"
 
 	sut := timeInputFormatter{
@@ -50,7 +53,7 @@ func Test_ToTime_hhmmss_local(t *testing.T) {
 	}
 }
 
-func Test_ToTime_success_hhmm(t *testing.T) {
+func Test_timeInputFormatter_ToTime_success_hhmm(t *testing.T) {
 	value := "02:02"
 
 	sut := timeInputFormatter{
@@ -71,5 +74,31 @@ func Test_ToTime_success_hhmm(t *testing.T) {
 	}
 	if sec != 0 {
 		t.Errorf("unexpected sec: %d", sec)
+	}
+}
+
+func Test_timeInputFormatter_ToTimeStruct(t *testing.T) {
+	input := "10:45"
+	sut := timeInputFormatter{
+		input: input,
+	}
+
+	v := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	actual := sut.ToTimeStruct(v)
+
+	if actual.Value != input {
+		t.Errorf("unexpected Value (%s)", actual.Value)
+	}
+	if actual.DateTime != "2009-11-10T23:00:00Z" {
+		t.Errorf("unexpected DateTimelue (%s)", actual.DateTime)
+	}
+	if actual.LocalDateTime == "" {
+		t.Error("unexpected LocalDateTime")
+	}
+	if actual.Unixtime != 1257894000 {
+		t.Errorf("unexpected Unixtime (%d)", actual.Unixtime)
+	}
+	if actual.UnixtimeMillis != 1257894000000 {
+		t.Errorf("unexpected Unixtime (%d)", actual.UnixtimeMillis)
 	}
 }
